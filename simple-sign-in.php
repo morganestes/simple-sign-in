e955ca0eba21c72a6bafc1fe03c4db9127dc264a
Morgan Estes <morgan.estes@gmail.com>
2012-06-02 00:58:21 -0500

    Formatted HTML

--- simple-sign-in.php
+++ simple-sign-in.php
@@ -48,56 +48,45 @@ function add_signin_scripts() {
 
 function show_simple_signin() {
 ?>
-           <div id="signin-wrapper">
-            <div id="signin-topnav">
-                <a href="#" class="signin-link"><span>Members, sign in here</span></a>
-            </div>
-            <div id="signin-menu">
-                <form method="post" id="simple-sign-in" name="signin" action="https://secure.okbar.org/logon">
-                    <label for="user">
-                        Bar #
-                    </label>
-                    <input id="user" name="user" value="" title="Bar Number" tabindex="4"
-                    type="text">
-                    </p>
-                    <p>
-                        <label for="password">
-                            Password
-                        </label>
-                        <input id="password" name="password" value="" title="Password" tabindex="5"
+<div class="ssi-wrapper">
+    <div id="ssi-top">
+        <a href="#" class="ssi-toggle">
+            <span>Members, sign in here</span>
+        </a>
+    </div>
+    <div class="ssi-form-wrapper">
+        <form method="post" id="simple-sign-in" name="signin" action="https://secure.okbar.org/logon">
+            <label for="ssi-user">Bar #</label>
+            <input id="ssi-user" name="user" value="" title="Bar Number" tabindex="4"
+                    type="text"></p>
+        <p>
+            <label for="ssi-password">Password</label>
+            <input id="ssi-password" name="password" value="" title="Password" tabindex="5"
                         type="password">
-                        <input id="show" type="checkbox" />
-                        <label>
-                            Show password
-                        </label>
-                    </p>
-                    <br>
-                    <p>
-                        <input type="radio" name="whereto" id="fastcase" value="fastcase" title="Sign in to Fastcase"
+            <input id="ssi-show-password" type="checkbox" />
+            <label for="ssi-show-password">Show password</label>
+        </p>
+        <br>
+        <p>
+            <input type="radio" name="whereto" id="fastcase" value="fastcase" title="Sign in to Fastcase"
                         checked="checked" />
-                        <label for="fastcase" title="Sign in to Fastcase">
-                            <strong>
-                                Fastcase
-                            </strong>
-                        </label>
-                        &emsp;
-                        <input type="radio" name="whereto" id="my" value="my" title="Sign in to myOKBar"
+            <label for="fastcase" title="Sign in to Fastcase"> <strong>Fastcase</strong>
+            </label>
+            &emsp;
+            <input type="radio" name="whereto" id="my" value="my" title="Sign in to MyOKBar"
                         />
-                        <label for="my" title="Sign in to myOKBar">
-                            <strong>
-                                myOKBar
-                            </strong>
-                        </label>
-                    </p>
-                    <br>
-                    <p>
-                        <input value="Sign in" tabindex="6" type="submit" onclick="submitForm();"
+            <label for="my" title="Sign in to MyOKBar"> <strong>MyOKBar</strong>
+            </label>
+        </p>
+        <br>
+        <p>
+            <input value="Sign in" tabindex="6" type="submit" onclick="check_whereto();"
                         />
-                    </p>
-                    <p class="forgot">
-                        <a href="https://secure.okbar.org/logon/okbarpass" id="resend-password-link">Forgot your password?</a>
-                    </p>
-                </form>
-            </div>
-        </div>
+        </p>
+    </form>
+    <p class="forgot">
+        <a href="https://secure.okbar.org/logon/okbarpass">Forgot your password?</a>
+    </p>
+</div>
+</div>
 <?php } ?>
