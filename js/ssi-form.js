9a25e1a72ad824a19f58e448a5c1651483569805
Morgan Estes <morgan.estes@gmail.com>
2012-06-02 00:22:18 -0500

    Added localStorage support to save username
    Combined http://diveintohtml5.info/storage.html and
    http://www.okbar.org/barnum.js to add saving username in browsers that
    support HTML5 localStorage.

--- js/ssi-form.js
+++ js/ssi-form.js
@@ -1,54 +1,81 @@
-jQuery(document).ready(function($) {
+jQuery(function($) {
 	var base_path = '/wordpress/wp-content/plugins/simple-sign-in/';
 	// let them see what they're typing in for the password
-	$('#password').showPassword('#show');
+	$('#simple-sign-in :password').showPassword('#ssi-show-password');
 
 	// handle the dropdown and show the form
-	$('.signin-link').click(function(e) {
+	$('.ssi-toggle').click(function(e) {
 		e.preventDefault();
-		$('#signin-menu').toggle();
-		$('.signin-link').toggleClass('menu-open');
+		// pre-fill the bar number field on before opening
+		if (localStorage.ssi_user !== null) {
+			$("#ssi-user").val(localStorage.ssi_user);
+		}
+		$('.ssi-form-wrapper').toggle();
+		$('.ssi-toggle').toggleClass('menu-open');
 
-
-			// open PIN reset options in modal window
-			$('a#resend-password-link').openDOMWindow({
-				draggable: 1,
-				height: 400,
-				width: 700,
-				eventType: 'click',
-				windowSource: 'iframe',
-				windowPadding: 10,
-				loader: 1,
-				loaderImagePath: 'images/ajax-loader.gif',
-				// relative path to the image needed
-				loaderHeight: 48,
-				loaderWidth: 48
-			}); // end modal
+		// open PIN reset options in modal window
+		$('.forgot a').openDOMWindow({
+			draggable: 1,
+			height: 400,
+			width: 700,
+			eventType: 'click',
+			windowSource: 'iframe',
+			windowPadding: 10,
+			loader: 1,
+			loaderImagePath: 'images/ajax-loader.gif',
+			// relative path to the image needed
+			loaderHeight: 48,
+			loaderWidth: 48
+		}); // end modal
 	}); // end sign-in display
-
 	// don't fire the submit if you're just closing the form
-	$('#signin-menu').mouseup(function() {
+	$('.ssi-form-wrapper').mouseup(function() {
 		return false;
 	});
 
 	// hide the form once it's submitted
 	$(document).mouseup(function(e) {
-		if ($(e.target).parent('a.signin-link').length === 0) {
-			$('.signin-link').removeClass('menu-open');
-			$('#signin-menu').hide();
+		if ($(e.target).parent('a.ssi-toggle').length === 0) {
+			$('.ssi-toggle').removeClass('menu-open');
+			$('.ssi-form-wrapper').hide();
 		}
 	});
 
-function submitForm() {
+
+});
+
+function check_whereto() {
 	var ssi_form = $('#simple-sign-in'),
 		form_url = ssi_form.attr('action'),
 		whereto = $('input[name="whereto"]:checked').val();
 
 	// if they're going to Fastcase, validate their info and send them on
 	if (whereto === 'fastcase') {
-		ssi_form.append('<input id="url" name="url" type="hidden" value="http://my.okbar.org/Fastcase" />');
+		ssi_form.append('<input id="url" name="url" type="hidden" value="http://my.okbar.org/Fastcase">');
 	}
-	// else, keep on truckin' and submit the form
+	// now keep on truckin' and submit the form
 	ssi_form.submit();
+
+	// see if we can save the user number for the next time
+	ssi_save_user();
 }
-});
+
+function supports_html5_storage() {
+	try {
+		return 'localStorage' in window && window['localStorage'] !== null;
+	} catch (e) {
+		return false;
+	}
+}
+
+function ssi_save_user() {
+	if (!supports_html5_storage()) {
+		console.log('no localStorage');
+		return false;
+	} else {
+		$("#simple-sign-in").submit(function() {
+			localStorage.ssi_user = $("#ssi-user").val();
+			console.log(localStorage.ssi_user);
+		});
+	}
+}
